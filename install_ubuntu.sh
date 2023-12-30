d91a98443dd09ba47960944ff7e60cdfd4fbc54c
quant42 <ga29qal@mytum.de>
2023-12-30 17:55:23 +0100

    Installation script for ubuntu

---
+++ install_ubuntu.sh
@@ -0,0 +1,33 @@
+# C++ Lua Perl Neko Python NodeJS Java
+# std, full, no
+
+# install haxe
+apt-get install haxe
+
+# for c++
+haxelib setup
+haxelib install hxcpp
+
+# for lua
+apt-get install lua5.4
+apt-get install luarocks
+luarocks install luautf8
+apt install libpcre3 libpcre3-dev
+luarocks install bit32
+luarocks install lrexlib-pcre
+
+# for perl
+apt-get install perl
+
+# for neko
+
+# for python
+
+# for nodejs
+apt install nodejs
+haxelib install hxnodejs
+
+# for java
+apt install java-common
+
+
