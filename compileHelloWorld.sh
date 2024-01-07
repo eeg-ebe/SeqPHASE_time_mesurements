7db07314298f95d9aab2e4fe274da63db8738760
quant42 <ga29qal@mytum.de>
2024-01-07 18:27:43 +0100

    Compile hello world script should create an own dir

--- compileHelloWorld.sh
+++ compileHelloWorld.sh
@@ -1,6 +1,8 @@
-haxe -main haxe.HelloWorld -cpp helloworld
-haxe -main haxe.HelloWorld -lua helloworld.lua
-haxe -main haxe.HelloWorld -neko helloworld.n
-haxe -main haxe.HelloWorld -python helloworld.py
-haxe -main haxe.HelloWorld -lib hxnodejs -js helloworld.js
-haxe -main haxe.HelloWorld -java helloworld.jar
+mkdir -p helloworld
+
+haxe -main haxe.HelloWorld -cpp helloworld/helloworld
+haxe -main haxe.HelloWorld -lua helloworld/helloworld.lua
+haxe -main haxe.HelloWorld -neko helloworld/helloworld.n
+haxe -main haxe.HelloWorld -python helloworld/helloworld.py
+haxe -main haxe.HelloWorld -lib hxnodejs -js helloworld/helloworld.js
+haxe -main haxe.HelloWorld -java helloworld/helloworld.jar
