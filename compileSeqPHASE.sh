59b63c0769278608c458e90ce7bfab7e70a32248
quant42 <ga29qal@mytum.de>
2024-01-08 09:37:43 +0100

    fix compileSeqPHASE.sh

--- compileSeqPHASE.sh
+++ compileSeqPHASE.sh
@@ -17,9 +17,9 @@ haxe -main SeqPhase1 -dce full -python seqphase1_dce_full.py
 haxe -main SeqPhase1 -dce std -python seqphase1_dce_std.py
 haxe -main SeqPhase1 -dce no -python seqphase1_dce_no.py
 
-haxe -main SeqPhase1 -dce full -js seqphase1_dce_full.py -lib hxnodejs
-haxe -main SeqPhase1 -dce std -js seqphase1_dce_std.py -lib hxnodejs
-haxe -main SeqPhase1 -dce no -js seqphase1_dce_no.py -lib hxnodejs
+haxe -main SeqPhase1 -dce full -js seqphase1_dce_full.js -lib hxnodejs
+haxe -main SeqPhase1 -dce std -js seqphase1_dce_std.js -lib hxnodejs
+haxe -main SeqPhase1 -dce no -js seqphase1_dce_no.js -lib hxnodejs
 
 haxe -main SeqPhase1 -dce full -java seqphase1_dce_full.jar
 haxe -main SeqPhase1 -dce std -java seqphase1_dce_std.jar
@@ -42,9 +42,9 @@ haxe -main SeqPhase2 -dce full -python seqphase2_dce_full.py
 haxe -main SeqPhase2 -dce std -python seqphase2_dce_std.py
 haxe -main SeqPhase2 -dce no -python seqphase2_dce_no.py
 
-haxe -main SeqPhase2 -dce full -js seqphase2_dce_full.py -lib hxnodejs
-haxe -main SeqPhase2 -dce std -js seqphase2_dce_std.py -lib hxnodejs
-haxe -main SeqPhase2 -dce no -js seqphase2_dce_no.py -lib hxnodejs
+haxe -main SeqPhase2 -dce full -js seqphase2_dce_full.js -lib hxnodejs
+haxe -main SeqPhase2 -dce std -js seqphase2_dce_std.js -lib hxnodejs
+haxe -main SeqPhase2 -dce no -js seqphase2_dce_no.js -lib hxnodejs
 
 haxe -main SeqPhase2 -dce full -java seqphase2_dce_full.jar
 haxe -main SeqPhase2 -dce std -java seqphase2_dce_std.jar
